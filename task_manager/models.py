7770a43dcf0b0d79109b55255d9fa525ba99f30e
Ihor <andreiunderwhat@mail.com>
2025-05-23 13:18:15 +0300

    add db and create models

--- task_manager/models.py
+++ task_manager/models.py
@@ -1,3 +1,49 @@
+from django.contrib.auth.models import AbstractUser
 from django.db import models
+from django.urls import reverse
 
-# Create your models here.
+
+class Position(models.Model):
+    name = models.CharField(max_length=100)
+
+    def __str__(self):
+        return self.name
+
+
+class TaskType(models.Model):
+    name = models.CharField(max_length=100)
+
+    def __str__(self):
+        return self.name
+
+
+class Worker(AbstractUser):
+    position = models.ForeignKey(Position, on_delete=models.CASCADE, related_name="workers")
+
+    def __str__(self):
+        return f"{self.username}: {self.position.name}"
+
+
+class Task(models.Model):
+    class Priority(models.TextChoices):
+        URGENT = "Urgent"
+        HIGH = "High"
+        MEDIUM = "Medium"
+        LOW = "Low"
+    name = models.CharField(max_length=100)
+    description = models.TextField()
+    deadline = models.DateTimeField()
+    is_completed = models.BooleanField(default=False)
+    priority = models.CharField(
+        max_length=10,
+        choices=Priority.choices,
+        default=Priority.MEDIUM
+    )
+    task_type = models.ForeignKey(TaskType, on_delete=models.CASCADE, related_name="tasks")
+    assignees = models.ManyToManyField(Worker, related_name='assigned_tasks')
+
+    def __str__(self):
+        return f"{self.name}: {self.task_type.name}, {self.priority}, {self.deadline}"
+
+    def get_absolute_url(self):
+        return reverse("task-detail", kwargs={"pk": self.pk})
