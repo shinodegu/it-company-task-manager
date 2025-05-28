71d96ca0f4921159a8edffe53e2f895e735e500e
Ihor <andreiunderwhat@mail.com>
2025-05-28 21:27:08 +0300

    Added an empty file with forms. Made paths for viewing the task list and viewing the task in detail

--- task_manager/urls.py
+++ task_manager/urls.py
@@ -1,6 +1,11 @@
 from django.urls import path
-from .views import IndexView
+from .views import (IndexView,
+                    TaskListView, TaskDetailView)
 
 urlpatterns = [
     path("", IndexView.as_view(), name="index"),
+    path("tasks/", TaskListView.as_view(), name="task-list"),
+    path("task/<int:pk>/", TaskDetailView.as_view(), name="task-detail"),
 ]
+
+app_name = "task_manager"
--- task_manager/views.py
+++ task_manager/views.py
@@ -9,3 +9,24 @@ from .models import Position, TaskType, Worker, Task
 
 class IndexView(generic.TemplateView):
     template_name = "task_manager/index.html"
+
+
+class TaskListView(generic.ListView):
+    model = Task
+    queryset = Task.objects.select_related(
+        "task_type"
+    ).prefetch_related(
+        "assignees",  # ✅ ManyToMany → prefetch
+        "tags"
+    )
+    paginate_by = 10
+
+
+class TaskDetailView(generic.DetailView):
+    model = Task
+    queryset = Task.objects.select_related(
+        "task_type"
+    ).prefetch_related(
+        "assignees",
+        "tags"
+    )
