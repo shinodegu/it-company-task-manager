6a726d1e371f77c0ef957f189ad98bb611bfa0d9
Ihor <andreiunderwhat@mail.com>
2025-06-08 20:47:49 +0300

    wrote forms for the search line for workers and tasks. Made it possible to select several employees and tags when creating and updating a task, adding CheckboxSelectMultiple inside ModelMultipleChoiceField to make it more convenient for the user

--- task_manager/forms.py
+++ task_manager/forms.py
@@ -3,10 +3,31 @@ from django.contrib.auth import get_user_model
 from django.contrib.auth.forms import UserCreationForm
 from django.core.exceptions import ValidationError
 
-from .models import Task, TaskType, Worker
+from .models import (Task, TaskType,
+                     Worker, Tag, Position)
 
 
 class TaskForm(forms.ModelForm):
+    assignees = forms.ModelMultipleChoiceField(
+        queryset=Worker.objects.all(),
+        widget=forms.CheckboxSelectMultiple(
+            attrs={
+                "class": "form-multiselect block w-full mt-1 rounded-lg border-gray-300"
+            }
+        ),
+        required=False,
+    )
+
+    tags = forms.ModelMultipleChoiceField(
+        queryset=Tag.objects.all(),
+        widget=forms.CheckboxSelectMultiple(
+            attrs={
+                "class": "form-multiselect block w-full mt-1 rounded-lg border-gray-300"
+            }
+        ),
+        required=False,
+    )
+
     class Meta:
         model = Task
         fields = [
@@ -21,10 +42,16 @@ class TaskForm(forms.ModelForm):
         ]
         widgets = {
             "deadline": forms.DateTimeInput(attrs={"type": "datetime-local"}),
-            "assignees": forms.CheckboxSelectMultiple,
-            "tags": forms.CheckboxSelectMultiple,
         }
 
+    def __init__(self, *args, **kwargs):
+        super().__init__(*args, **kwargs)
+
+        # Настраиваем отображение имени сотрудника
+        self.fields["assignees"].label_from_instance = lambda obj: f"{obj.username}: {obj.position.name}"
+
+        # Если хочешь для тегов тоже форматировать отображение:
+        self.fields["tags"].label_from_instance = lambda obj: obj.name
 
 class WorkerForm(UserCreationForm):
     class Meta(UserCreationForm.Meta):
@@ -41,3 +68,36 @@ class WorkerPositionUpdateForm(forms.ModelForm):
     class Meta:
         model = Worker
         fields = ["position"]
+
+
+class PositionDeleteForm(forms.Form):
+    positions = forms.ModelMultipleChoiceField(
+        queryset=Position.objects.all(),
+        widget=forms.CheckboxSelectMultiple,
+        required=True
+    )
+
+
+class WorkerSearchForm(forms.Form):
+    title = forms.CharField(
+        max_length=255,
+        required=False,
+        label="",
+        widget=forms.TextInput(
+            attrs={
+                "placeholder": "Search by username",
+            })
+    )
+
+
+class TaskSearchForm(forms.Form):
+    title = forms.CharField(
+        max_length=255,
+        required=False,
+        label="",
+        widget=forms.TextInput(
+            attrs={
+                "placeholder": "Search by name",
+            })
+    )
+
